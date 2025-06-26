2060c6b30b0b98f878f50854cb2877728ba6d5ea
Gabriel29306 <ggkervran@outlook.com>
2025-06-26 21:56:16 +0200

    refactor: remove cli part

--- anime_sama_api/__init__.py
+++ anime_sama_api/__init__.py
@@ -4,25 +4,7 @@ from .season import Season
 from .episode import Episode, Languages, Players
 from .langs import Lang, LangId, lang2ids, id2lang, flags
 
-try:
-    from .cli.__main__ import main
-    from .cli.downloader import download, multi_download
-except ImportError:
-    import sys
-
-    def main() -> int:
-        print(
-            "This anime-sama_api function could not run because the required "
-            "dependencies were not installed.\nMake sure you've installed "
-            "everything with: pip install 'anime-sama_api[cli]'"
-        )
-
-        sys.exit(1)
-
-    download = multi_download = main  # type: ignore
-
-
-# __package__ = "anime-sama_api"
+__package__ = "anime-sama_api"
 __all__ = [
     "AnimeSama",
     "Catalogue",
@@ -34,13 +16,7 @@ __all__ = [
     "LangId",
     "lang2ids",
     "id2lang",
-    "flags",
-    "download",
-    "multi_download",
-    "main",
+    "flags"
 ]
 
-"""__locals = locals()
-for __name in __all__:
-    if not __name.startswith("__"):
-        setattr(__locals[__name], "__module__", "anime-sama_api")  # noqa"""
+__locals = locals()
