ad116c97c9bb84edd56837611713ac7c292c6bc9
Gabriel29306 <ggkervran@outlook.com>
2025-06-26 23:33:09 +0200

    chore: remove unused test code

--- anime_sama_apis/langs.py
+++ anime_sama_apis/langs.py
@@ -41,38 +41,3 @@ flagid2lang: dict[FlagId, Lang] = {
 for language, language_ids in lang2ids.items():
     for lang_id in language_ids:
         flags[lang_id] = flags[language]
-
-
-if __name__ == "__main__":
-    import re
-    import asyncio
-    from pprint import pprint
-
-    import httpx
-
-    from anime_sama_apis.top_level import AnimeSama
-
-    SCRIPT_VIDEO_URL = "https://anime-sama.fr/js/contenu/script_videos.js"
-    page = httpx.get(SCRIPT_VIDEO_URL).text
-    langs = {}
-
-    matchs = re.findall(r"if\((.+)\){langue = \"(.+)\";}", page)
-    for match in matchs:
-        langs[match[1]] = match[0].split('"')[1::2]
-
-    pprint(langs)
-
-    async def main():
-        async for catalogue in AnimeSama("https://anime-sama.fr/").catalogues_iter():
-            if await catalogue.seasons():
-                break
-        else:
-            raise
-
-        vostfr_url = (await catalogue.seasons())[0].url + "vostfr/"
-        response = await catalogue.client.get(vostfr_url)
-        if not response.is_success:
-            raise
-        print(set(re.findall(r"src=\".+flag_(.+?)\.png\"", response.text)))
-
-    asyncio.run(main())
